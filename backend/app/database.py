c55a0b3423e578cfc580e992fce921c75c2534ad
Sreya V <sreyavenkat80@gmail.com>
2026-03-23 19:31:31 +0530

    Saving old changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,10 @@
 from supabase import create_client
+from dotenv import load_dotenv
 import os
 import uuid
 from datetime import datetime
+
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
